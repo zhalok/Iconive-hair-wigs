f0287501a52f9e6a2c4b8951f70e2cd51d069757
zhalok <zhalok>
2023-04-19 05:04:44 +0600

    experiment

--- src/Components/ProductDetails/ProductDetails.js
+++ src/Components/ProductDetails/ProductDetails.js
@@ -148,6 +148,7 @@ export default function ProductDetails({ id }) {
                                     const idx_name = newState
                                       .map((e) => e.name)
                                       .indexOf(f.name);
+                                    console.log(idx_name);
                                     if (idx_name != -1) {
                                       prev[idx_name]._id = f._id;
                                     } else {
