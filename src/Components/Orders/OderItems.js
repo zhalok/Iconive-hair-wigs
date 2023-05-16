9f94e7f75fb205fb5e73168749d4748ee7055637
zhalok <=>
2023-05-17 01:25:44 +0600

    worked on orders

--- src/Components/Orders/OderItems.js
+++ src/Components/Orders/OderItems.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "../../utils/axios";
 
-export default function OrderItem({ orderItemId }) {
-  return <div>
-    
-  </div>;
+export default function OrderItem({ orderItem }) {
+  const [product, setProduct] = useState({});
+  const getProduct = async () => {
+    try {
+      const response = await axios.get(`/products/${orderItem.product}`);
+      console.log("product", response.data);
+      setProduct(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  useEffect(() => {
+    getProduct();
+  }, []);
+  return (
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        rowGap: "10px",
+      }}
+    >
+      <h2>Name: {product.name}</h2>
+      <div>Amount: {orderItem.amount}</div>
+      <div></div>
+    </div>
+  );
 }
