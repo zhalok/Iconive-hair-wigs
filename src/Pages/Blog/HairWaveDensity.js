f60fbe2860d78aa593a65f8d50dce5dd24f23ac0
Md-Abdul-Kadir <abdulkaderakash08@gmail.com>
2023-09-25 23:37:19 +0600

    addded code for guide me

--- src/Pages/Blog/HairWaveDensity.js
+++ src/Pages/Blog/HairWaveDensity.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from "react";
 import banner from "../../.././src/Components/Images/joinus/blog.webp";
 import "./Blog.css";
@@ -58,86 +59,58 @@ export default function HairWaveDensity() {
             aria-labelledby="nav-home-tab"
           >
             <div className="text-start">
-              <h5 className="fw-bold pb-3">
-                How to Wear a Toupee at Home by Yourself{" "}
-              </h5>
-              <p className="text-theme-gray fs-6 ">
-                For those who are new to wearing hairpieces, we understand that
-                it can be a bit challenging to know how to properly attach and
-                style them. That's why we have created a helpful video tutorial
-                that demonstrates a simple method for securing your hair system
-                at home. However, if you're a beginner and prefer professional
-                assistance, we recommend visiting a barber shop. Barbers are
-                experienced in working with hair systems and can provide expert
-                advice, including customizing the system to match your head
-                shape and creating a stylish haircut. s
-              </p>
+              <h5 className="fw-bold pb-3 mt-4 border-bottom">Wave-Curl</h5>
+              <div className="w-100">
+                <img
+                  className="w-100"
+                  src="/Image/blog/wave.jpg"
+                  alt="This is an image"
+                />
+              </div>
 
-              <h6 className="fw-bold py-2 pt-4">
-                To attach a lace hair system, follow these three easy steps::
-              </h6>
-              <ul>
-                <li className="text-gray text-16">
-                  Prepare the area by shaving the top of your head to create a
-                  smooth surface where the adhesive will be applied.
-                </li>
-                <li className="text-gray text-16">
-                  Apply the adhesive, which can be glue or tape, either on your
-                  scalp or directly on the hair system.
-                </li>
-                <li className="text-gray text-16">
-                  Position the hair system by starting at the center of your
-                  forehead and gently sticking the tip of the system. Gradually
-                  roll back the system, ensuring it is even on both sides, until
-                  it sits nicely at the back and covers the shaved area.
-                </li>
-              </ul>
-              <p>
-                Once attached, you can style your hair system as desired.
-                Whether you choose to follow our tutorial or seek professional
-                assistance, we are here to support you in achieving a natural
-                and confident look.
-              </p>
-
-              <h5 className="fw-bold pb-3 pt-5">
-                How Would I know What Size to Buy?
-              </h5>
-              <p className="text-theme-gray">
-                When it comes to the base size of toupees, they are available in
-                various dimensions such as 6x8, 7x8, 7x9, 8x10, and other sizes.
-                It's important for you to determine the length and width of your
-                hair loss area before selecting the right size.
-                <br />
-                To begin, locate your front hairline. Typically, it is
-                approximately four fingers' width away from your eyebrow. Place
-                four fingers on your forehead, ensuring that one side aligns
-                with your eyebrow and the other side marks the starting point of
-                your front hairline.
-              </p>
-
-              <h6 className="fw-bold py-2 pt-4">Next, ascertain the length:</h6>
-              <ul>
-                <li className="text-gray text-16">
-                  Measure from your front hairline to the point where your hair
-                  loss ends at the back of your head.
-                </li>
-              </ul>
-
-              <h6 className="fw-bold py-2 pt-4">
-                Lastly, determine the width:{" "}
-              </h6>
-              <ul>
-                <li className="text-gray text-16">
-                  Measure the widest area of your hair loss region from one side
-                  to the other.
-                </li>
-              </ul>
-
-              <p className="pt-4 tex">
-                By accurately measuring the length and width of your hair loss
-                area, you can ensure that the toupee you choose fits you
-                perfectly and provides the desired coverage.
-              </p>
+              <h5 className="fw-bold pb-3 pt-5 mt-5 border-bottom">Density</h5>
+              <div className="w-100 d-flex justify-content-center pt-5">
+                <div className="text-center w-19">
+                  <img
+                    className="w-100"
+                    src="/Image/blog/extra ight.png"
+                    alt="This is an image"
+                  />
+                  <p className="fs-5 pt-2">Extra light</p>
+                </div>
+                <div className="text-center w-19">
+                  <img
+                    className="w-100"
+                    src="/Image/blog/light.png"
+                    alt="This is an image"
+                  />
+                  <p className="fs-5 pt-2">Light</p>
+                </div>
+                <div className="text-center w-19">
+                  <img
+                    className="w-100"
+                    src="/Image/blog/medium heavy.png"
+                    alt="This is an image"
+                  />
+                  <p className="fs-5 pt-2">Medium heavy</p>
+                </div>
+                <div className="text-center w-19">
+                  <img
+                    className="w-100"
+                    src="/Image/blog/medium light.png"
+                    alt="This is an image"
+                  />
+                  <p className="fs-5 pt-2">Medium light</p>
+                </div>
+                <div className="text-center w-19">
+                  <img
+                    className="w-100"
+                    src="/Image/blog/medium.png"
+                    alt="This is an image"
+                  />
+                  <p className="fs-5 pt-2">Medium</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
